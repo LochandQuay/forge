a88aacb997fe94446028b70795e6670a1b980dcf
Hope <hopewanroy@gmail.com>
2017-10-05 14:41:19 -0700

    add basic devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    devise_for :users, controllers: {
+      sessions: 'api/users/sessions', 
+      passwords: 'api/users/passwords',
+      registrations: 'api/users/registrations',
+      unlocks: 'api/users/unlocks'
+    }
+  end
+
+  root to: 'static_pages#root'
 end
